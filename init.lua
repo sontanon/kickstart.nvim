87efc784bd9298906e00fad960c1ed4d3f5686ba
Santiago Ontanon <sontanon@deloitte.com>
2026-04-11 15:38:47 -0500

    Fix deprecated vim.lsp.with() calls for Neovim 0.12

--- init.lua
+++ init.lua
@@ -81,17 +81,13 @@ vim.o.scrolloff = 3
 vim.o.confirm = true
 
 
--- [[ Configure LSP Hover Handler ]]
--- Fix markdown rendering in hover documentation
--- This prevents escaped underscores (\_) in Python docstrings and other markdown content
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = 'rounded',
-})
-
--- Also fix signature help rendering
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = 'rounded',
-})
+-- Rounded borders for hover and signature help popups
+vim.lsp.handlers['textDocument/hover'] = function(err, result, ctx, config)
+  return vim.lsp.handlers.hover(err, result, ctx, vim.tbl_extend('force', config or {}, { border = 'rounded' }))
+end
+vim.lsp.handlers['textDocument/signatureHelp'] = function(err, result, ctx, config)
+  return vim.lsp.handlers.signature_help(err, result, ctx, vim.tbl_extend('force', config or {}, { border = 'rounded' }))
+end
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
